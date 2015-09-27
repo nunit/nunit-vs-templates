71a2e5d1dafe745c52a664b12fd47bedac088bab
Rob Prouse <rob@prouse.org>
2015-09-27 15:50:19 -0400

    Added test fixture item templates for C# and VB

---
+++ nunit.templates/ItemTemplates/Test/NUnit Test Fixture/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $safeitemname$
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
