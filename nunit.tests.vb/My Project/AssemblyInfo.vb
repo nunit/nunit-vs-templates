c033b8a15a1665baf8325c2c624596d33916fcf3
Chris Maddock <chrismaddock@live.co.uk>
2017-04-04 13:07:39 +0100

    Prepare for v1.3 release

--- nunit.tests.vb/My Project/AssemblyInfo.vb
+++ nunit.tests.vb/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("nunit.tests.vb")> 
-<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyCopyright("Copyright © 2017")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
