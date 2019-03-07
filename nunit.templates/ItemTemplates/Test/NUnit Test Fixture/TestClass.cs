55b5dee940b5d8b959a8ee7370205e1f2dda91ab
Terje Sandstrom <terje@hermit.no>
2019-03-07 14:52:01 +0100

    Added back ItemTemplates folder

---
+++ nunit.templates/ItemTemplates/Test/NUnit Test Fixture/TestClass.cs
@@ -0,0 +1,17 @@
+﻿// NUnit3 Tests,  see documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation 
+using NUnit.Framework;
+
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $safeitemname$
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 43;
+            Assert.That(answer,Is.EqualTo(42),"Some useful error message");
+        }
+    }
+}
