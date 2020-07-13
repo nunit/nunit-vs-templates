00185cec1aa43630825dfc0aed3761aefaf18f51
Terje Sandstrøm <terje@hermit.no>
2020-07-13 22:21:17 +0200

    Updated to NUnit 3.12 and adapter 3.17

--- nunit.tests.core.csharp/TestClass.cs
+++ nunit.tests.core.csharp/TestClass.cs
@@ -1,7 +1,5 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace $safeprojectname$
